fcb1450cd251ab71d5ed58510d2176dbc33fa1c9
Mayank <mayanktyagi13jun@gmail.com>
2023-10-10 23:38:06 +0530

    Added Utility function

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,20 +1,28 @@
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 module.exports.generateHash = async (password) => {
+  const salt = await bcrypt.genSalt(10);
 
-    const salt = await bcrypt.genSalt(10);
+  const hash = await bcrypt.hash(password, salt);
 
-    const hash =  await bcrypt.hash(password, salt);
-
-    return hash;
-
-}
+  return hash;
+};
 
 module.exports.isPasswordValid = async (password, hash) => {
-try {
-   return await bcrypt.compare(password, hash);
-} catch (error) {
+  try {
+    return await bcrypt.compare(password, hash);
+  } catch (error) {
     throw new Error("Incorrect email or Password");
-}
+  }
+};
 
+module.exports.generateToken = async (payload) => {
+    try {
+        return jwt.sign(payload, process.env.JWT_SECRET_KEY, {
+            expiresIn: process.env.JWT_EXPIRES_IN,
+        });
+    }catch (error) {
+        throw new Error("Unable to generate token");
+    }
 }
